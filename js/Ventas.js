f0c15342510a056eb56c033cde4d0d8dfeba2901
“SoyManuelPerez” <Soy_ManuelPerez@outlook.com>
2023-02-07 13:30:22 -0500

    Creacion de bakeng

--- js/Ventas.js
+++ js/Ventas.js
@@ -1,5 +1,5 @@
 $(document).ready( function () {
-    $('#Ventas').DataTable({
+    $('#ventas').DataTable({
         dom: 'Bfrtip',
         buttons: [
             'excel', 'pdf', 'print'
@@ -7,3 +7,19 @@ $(document).ready( function () {
     } 
     );
 } );
+let Registro = JSON.parse(localStorage.getItem("Registro")) || [];
+function mostar() {
+    let html  = "";
+    Registro. forEach(({ id, Producto, Precio ,tipo,fecha}) => {
+        html += `
+            <tr>
+                <td >${Producto}</td>
+                <td>${Precio}</td>
+                <td>${tipo}</td>
+                <td>${fecha}</td>
+            </tr>
+        `;
+    });
+    contenido. innerHTML = html;
+}
+mostar();
